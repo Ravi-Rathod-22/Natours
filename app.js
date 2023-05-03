580571b0780e8deec239cfd0a34eb2e3e5620dfd
Ravi-Rathod-22 <raviarthod4449@gmail.com>
2023-05-04 01:08:37 +0530

    Create GET And POST Request

--- app.js
+++ app.js
@@ -1,25 +1,59 @@
-////////////////////////////////////////////////////////////////////////////////
-// Setting up Express and Basic Routing
-
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get(`/`, (req, res) => {
-  res
-    .status(200)
-    .json({ message: `Hello from the server side!`, app: `Natours` });
+app.use(express.json());
+
+// app.get(`/`, (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: `Hello from the server side!`, app: `Natours` });
+// });
+
+// app.post(`/`, (req, res) => {
+//     res.send(`You can post to this endpoint...`);
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'Success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
-app.post(`/`, (req, res) => {
-    res.send(`You can post to this endpoint...`);    
+app.post(`/api/v1/tours`, (req, res) => {
+  //  console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'Success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running at ${port}...`);
 });
-
 
 ////////////////////////////////////////////////////////////////////////////////
 // Starting Our API_ Handling GET Requests
