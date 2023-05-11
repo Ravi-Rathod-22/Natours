e27a29044ffc99aec51862a2bccee5d2c2152f7e
Ravi-Rathod-22 <raviarthod4449@gmail.com>
2023-05-11 23:36:45 +0530

    Install And Connecting Database with the Express And Creating a Simple Tour Model With Mongoose

--- server.js
+++ server.js
@@ -1,4 +1,36 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = require('./app');
+
+dotenv.config({ path: `./config.env`});
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+}).then((con) => {
+  console.log(`DB Connection successful!`);
+});
+
+const tourSchema = new mongoose.Schema({
+  name : {
+    type : String,
+    required: [true, 'A tour must have a name'],
+    unique : true
+  },
+  rating : {
+    type : Number,
+    default : 4.5
+  },
+  price:{
+    type : Number,
+    default : [true, 'A tor must have a price'],
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema)
 
 const port = 3000;
 app.listen(port, () => {
