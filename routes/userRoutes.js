8d35694a96ca3329c734bbfecbd6f6bc52414e38
Ravi-Rathod-22 <ravirathod4449@gmail.com>
2023-06-13 12:12:52 +0530

    Sending Emails with Nodemailer

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,10 +6,10 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/signup', authController.signup);
-router.get('/login', authController.login);
+router.post('/login', authController.login);
 
 router.post('/forgotPassword', authController.forgotPassword);
-router.get('/resetPassword', authController.resetPassword);
+// router.patch('/resetPassword/:token', authController.resetPassword);
 
 router
   .route(`/`)
