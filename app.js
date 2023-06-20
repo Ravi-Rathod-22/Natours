3d84cad07463210b0cd072e247ccd6eefbafbc13
Ravi-Rathod-22 <ravirathod4449@gmail.com>
2023-06-20 11:05:37 +0530

    Setting Security HTTP Headers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const app = express();
 
@@ -12,6 +13,9 @@ const tourRouter = require(`./routes/tourRoutes`);
 const userRouter = require(`./routes/userRoutes`);
 
 // MIDDLEWARE
+
+app.use(helmet());
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan(`dev`));
 }
@@ -24,7 +28,7 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
 
 app.use(cors());
 
