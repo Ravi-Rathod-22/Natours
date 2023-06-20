f1adcaeecaf67d19349040cd3e8b4e4b193eee05
Ravi-Rathod-22 <ravirathod4449@gmail.com>
2023-06-20 11:53:50 +0530

    Preventing Parameter Pollution

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const app = express();
 
@@ -30,7 +31,7 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 
-app.use(express.json({ limit: '10kb ' }));
+app.use(express.json());
 
 app.use(cors());
 
@@ -39,6 +40,12 @@ app.use(mongoSanitize());
 
 // Data sanitization against XSS
 app.use(xss());
+
+app.use(
+  hpp({
+    whitelist: ['duration', 'ratingsQuality', 'price', 'maxGroupSize'],
+  })
+);
 
 app.use(express.static(`${__dirname}/public`));
 
