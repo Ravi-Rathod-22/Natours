f5073ce7e6c322666513239b3a09c43f9c527fa5
Ravi-Rathod-22 <ravirathod4449@gmail.com>
2023-06-29 18:36:42 +0530

    Remove Secure true from auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,7 +19,6 @@ const createSendToken = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 60 * 60 * 1000
     ),
-    secure: true,
     httpOnly: true,
   };
 
