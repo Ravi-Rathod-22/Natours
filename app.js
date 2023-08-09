55cd1234f4745ee3669020fce3d0a1503023c09f
Ravi-Rathod-22 <ravirathod4449@gmail.com>
2023-08-09 12:40:10 +0530

    Extending Our Base Template with Blocks

--- app.js
+++ app.js
@@ -63,6 +63,18 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours',
+  });
+});
+
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
+  });
+});
+
 app.use((req, res, next) => {
   console.log(`Hello From Middleware`);
   next();
