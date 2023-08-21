d8646b86a682817ae0c01d2f52a21131d9fe336b
Ravi-Rathod-22 <ravirathod4449@gmail.com>
2023-08-21 14:34:07 +0530

    Including a Map with Mapbox - Part 2

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,4 +1,45 @@
-console.log('Hello From Client Side !');
+/* eslint-disable */
 
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
-console.log('locations', locations);
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoicmF2aS1yYXRob2QiLCJhIjoiY2xsa2d5OWM3MXQ0aTNkcWpxYTlsZTJpdSJ9.xj-6fANmTzuV7TvmcSwxdw';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/ravi-rathod/cllkhe5xk006201r50h9v1yzm',
+  scrollZoom: false,
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach((loc) => {
+  // Create marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  // Add marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom',
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  // Add popup
+  new mapboxgl.Popup({ offset: 30 })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description} </p>`)
+    .addTo(map);
+  // Extend map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100,
+  },
+});
