2a7a8fa315a09d1fd213546a43ecb6f64849bcbd
Ravi-Rathod-22 <ravirathod4449@gmail.com>
2023-08-23 11:45:20 +0530

    Logging in Users with Our API - Part 2

--- public/js/login.js
+++ public/js/login.js
@@ -1,15 +1,25 @@
 /* eslint-disable */
 
 const login = async (email, password) => {
-  const res = await axios({
-    method: 'POST',
-    url: 'http://127.0.0.1:3000/api/v1/users/login',
-    data: {
-      email,
-      password,
-    },
-  });
-  console.log(res);
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: 'http://127.0.0.1:3000/api/v1/users/login',
+      data: {
+        email,
+        password,
+      },
+    });
+
+    if (res.data.status === 'success') {
+      alert('Logged in successfully!');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
+    }
+  } catch (error) {
+    alert(error.response.data.message);
+  }
 };
 
 document.querySelector('.form').addEventListener('submit', (e) => {
